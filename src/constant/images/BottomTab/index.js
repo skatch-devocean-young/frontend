a67fa34f047d0e6832e55785c27da47f581e7065
Jihyun Lee <zzh23@naver.com>
2024-10-13 16:34:29 +0900

    [feat] : BottomTab 아이콘 변경 적용

--- src/constant/images/BottomTab/index.js
+++ src/constant/images/BottomTab/index.js
@@ -1,9 +1,9 @@
-import HomeIcon from "./Home/Home.png";
-import HomeFocusedIcon from "./Home/HomeFocused.png";
-import FeedIcon from "./Feed/Feed.png";
-import FeedFocusedIcon from "./Feed/FeedFocused.png";
-import MyPageIcon from "./MyPage/MyPage.png";
-import MyPageFocusedIcon from "./MyPage/MyPageFocused.png";
+import HomeIcon from "./Home/HomeIcon.png";
+import HomeFocusedIcon from "./Home/HomeFocusedIcon.png";
+import FeedIcon from "./Feed/FeedIcon.png";
+import FeedFocusedIcon from "./Feed/FeedFocusedIcon.png";
+import MyPageIcon from "./MyPage/MyPageIcon.png";
+import MyPageFocusedIcon from "./MyPage/MyPageFocusedIcon.png";
 
 export {
   HomeIcon,
